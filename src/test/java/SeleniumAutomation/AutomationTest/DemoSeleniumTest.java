29c69c400b6b23a206e0cb3c3bf7edc4030c775e
Liu <vliu@advent.com>
2019-12-13 18:33:32 +0800

    update

--- src/test/java/SeleniumAutomation/AutomationTest/DemoSeleniumTest.java
+++ src/test/java/SeleniumAutomation/AutomationTest/DemoSeleniumTest.java
@@ -1,5 +1,10 @@
 package SeleniumAutomation.AutomationTest;
 
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.AssertJUnit;
+import org.testng.ITestResult;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -23,7 +28,19 @@ public class DemoSeleniumTest {
 		driver.get("https://www.baidu.com");
 		Reporter.log("我马上要关闭浏览器了1111111111111111了");
 		Reporter.log("我马上要关闭浏览器了2222222222222222222");
-	    Assert.assertTrue(true);
+	    AssertJUnit.assertTrue(true);
 	    Reporter.log("我马上要打开浏览器了");
 	}
+	
+	@AfterMethod
+	 public void afterMethod(ITestResult testResult){
+        if (testResult.getStatus() == ITestResult.FAILURE){
+            System.out.println("Failed: "+testResult.getMethod().getMethodName());
+        }
+        if (testResult.getStatus()==ITestResult.SUCCESS){
+            System.out.println("Successed:"+testResult.getName());
+        }
+        
+ 
+    }
 }
